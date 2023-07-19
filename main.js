2f727a143838c69bd5a19f01588228a720469930
Bitha007 <bidheyakthapa321@gamil.com>
2023-07-19 22:44:12 +0545

    Add responsive nav bar

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ function downloadCV() {
 }
 
 document.getElementById("download-btn").addEventListener("click", downloadCV);
+
+const primaryNav = document.querySelector("#primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", "true");
+    navToggle.setAttribute("aria-expanded", "true");
+  } else {
+    primaryNav.setAttribute("data-visible", "false");
+    navToggle.setAttribute("aria-expanded", "false");
+  }
+});
