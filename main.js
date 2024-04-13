ce611221fd188884a953d34282d95d196406544a
bidheyakthapa <bidheyakthapa321@gamil.com>
2024-04-13 21:49:25 +0545

    Make minor changes for nav and some spacing

--- main.js
+++ main.js
@@ -14,6 +14,14 @@ document.getElementById("download-btn").addEventListener("click", downloadCV);
 
 const primaryNav = document.querySelector("#primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const navItem = document.querySelectorAll(".nav_link");
+
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    primaryNav.setAttribute("data-visible", "false");
+    navToggle.setAttribute("aria-expanded", "false");
+  });
+});
 
 navToggle.addEventListener("click", () => {
   const visibility = primaryNav.getAttribute("data-visible");
